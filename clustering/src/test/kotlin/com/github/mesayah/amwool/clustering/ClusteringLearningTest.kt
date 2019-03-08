45cb9e066a80406d29859b301379b098dd42b9a1
mesayah <7hjk8xy49abe1>
2019-03-08 15:22:16 +0100

    Last fixes

--- clustering/src/test/kotlin/com/github/mesayah/amwool/clustering/ClusteringLearningTest.kt
+++ clustering/src/test/kotlin/com/github/mesayah/amwool/clustering/ClusteringLearningTest.kt
@@ -1,15 +1,12 @@
 package com.github.mesayah.amwool.clustering
 
-import com.github.mesayah.amwool.mlcommon.AbstractLearningTest
-import com.github.mesayah.amwool.mlcommon.Learn
-import com.github.mesayah.amwool.mlcommon.PrepareData
-import com.github.mesayah.amwool.mlcommon.Save
+import com.github.mesayah.amwool.mlcommon.*
 import weka.clusterers.DensityBasedClusterer
 import weka.clusterers.EM
 
 class ClusteringLearningTest : AbstractLearningTest<DensityBasedClusterer>() {
     override val prepareDataTypeclassSupplier: () -> PrepareData<DensityBasedClusterer> = { PrepareDataForClustering() }
-    override val learnTypeclassSupplier: () -> Learn<DensityBasedClusterer> = { LearnClusterer() }
+    override val learnTypeclassSupplier: () -> Learn<DensityBasedClusterer> = { ClustererLearn() }
     override val saveTypeclassSupplier: () -> Save<DensityBasedClusterer> = { Save() }
     override val preapareParametersSupplier: () -> Array<Any> = { emptyArray() }
     override val modelSupplier: () -> DensityBasedClusterer = { EM() }
